91b62c0361f10a8b8de79c7889873b9a8299e34f
mktosos <taniguchidesigns@hotmail.com>
2019-01-14 20:14:22 -0800

    start btn and reveal and reset f() added

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$( function(){
+
+//Start button to start game and run game f()
+$("#startButton").on("click", function(){
+    reveal();
+    resetGame();
+});
+function reveal() {
+    $('.game').css('opacity', "1")
+
+    
+}
+function resetGame() {
+    
+}
+
+
+
+});
